474fc18442605571b5d7fc49337a420ed19d4f69
Rafael Uribe <rafael.uribeg@gmail.com>
2024-02-24 17:56:06 -0300

    Create Register User

--- frontend/job-search/src/pages/Register.js
+++ frontend/job-search/src/pages/Register.js
@@ -3,12 +3,45 @@ import React from 'react';
 
 const Register = () => {
 
+const [firstName, setFirstName] = useState('');
+const [lastName, setLastName] = useState('');
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
+const [role, setRole] = useState('Candidate');
+const [termsAccepted, setTermsAccepted] = useState(false);
 
+const handleSubmit = (event) => {
+    event.preventDefault();    
+};  
 
   return (
-    <div>
-      <h2>Register Page</h2>
-    </div>
+     <form onSubmit={handleSubmit}>
+            <label>
+                First Name:
+                <input type="text" value={firstName} onChange={e => setFirstName(e.target.value)} required />
+            </label>
+            <label>
+                Last Name:
+                <input type="text" value={lastName} onChange={e => setLastName(e.target.value)} required />
+            </label>
+            <label>
+                Email:
+                <input type="email" value={email} onChange={e => setEmail(e.target.value)} required />
+            </label>
+            <label>
+                Password:
+                <input type="password" value={password} onChange={e => setPassword(e.target.value)} required />
+            </label>
+            <label>
+                Rol:
+                <input type="radio" value="Candidate" checked={role === 'Candidate'} onChange={e => setRole(e.target.value)} /> Candidate
+                <input type="radio" value="Manager" checked={role === 'Manager'} onChange={e => setRole(e.target.value)} /> Manager
+            </label>
+            <label>
+                <input type="checkbox" checked={termsAccepted} onChange={e => setTermsAccepted(e.target.checked)} /> I agree to the Terms of Service and Privacy Policy
+            </label>
+            <button type="submit">Sign up</button>
+        </form>
   );
 };
 
