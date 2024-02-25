d38dde0ac0f5499b736a2a220906217572e5714b
Rafael Uribe <rafael.uribeg@gmail.com>
2024-02-24 21:20:40 -0300

    create RegisterForm

--- frontend/job-search/src/pages/Register.js
+++ frontend/job-search/src/pages/Register.js
@@ -1,48 +1,13 @@
 // Register.js
-import React from 'react';
+import React, { useState } from 'react';
+import RegisterForm from '../components/RegisterForm';
 
 const Register = () => {
-
-const [firstName, setFirstName] = useState('');
-const [lastName, setLastName] = useState('');
-const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
-const [role, setRole] = useState('Candidate');
-const [termsAccepted, setTermsAccepted] = useState(false);
-
-const handleSubmit = (event) => {
-    event.preventDefault();    
-};  
-
-  return (
-     <form onSubmit={handleSubmit}>
-            <label>
-                First Name:
-                <input type="text" value={firstName} onChange={e => setFirstName(e.target.value)} required />
-            </label>
-            <label>
-                Last Name:
-                <input type="text" value={lastName} onChange={e => setLastName(e.target.value)} required />
-            </label>
-            <label>
-                Email:
-                <input type="email" value={email} onChange={e => setEmail(e.target.value)} required />
-            </label>
-            <label>
-                Password:
-                <input type="password" value={password} onChange={e => setPassword(e.target.value)} required />
-            </label>
-            <label>
-                Rol:
-                <input type="radio" value="Candidate" checked={role === 'Candidate'} onChange={e => setRole(e.target.value)} /> Candidate
-                <input type="radio" value="Manager" checked={role === 'Manager'} onChange={e => setRole(e.target.value)} /> Manager
-            </label>
-            <label>
-                <input type="checkbox" checked={termsAccepted} onChange={e => setTermsAccepted(e.target.checked)} /> I agree to the Terms of Service and Privacy Policy
-            </label>
-            <button type="submit">Sign up</button>
-        </form>
-  );
+    return(
+        <div>
+            <RegisterForm></RegisterForm>
+        </div>
+    );
 };
 
 export default Register;
