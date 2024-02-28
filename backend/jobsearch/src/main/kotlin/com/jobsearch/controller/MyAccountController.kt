2b96f7f62f36c58ccb73c6f5aca531e01ca6be43
s0alken <ropedent@gmail.com>
2024-02-27 21:48:42 -0300

    added user to cv

---
+++ backend/jobsearch/src/main/kotlin/com/jobsearch/controller/MyAccountController.kt
@@ -0,0 +1,25 @@
+package com.jobsearch.controller
+
+import com.jobsearch.dto.CvRequestDTO
+import com.jobsearch.dto.CvResponseDTO
+import com.jobsearch.dto.VacancyDto
+import com.jobsearch.service.CvService
+import com.jobsearch.service.VacancyService
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/v1/my-account")
+@Validated
+class MyAccountController(
+    val cvService: CvService
+) {
+
+    @GetMapping("cvs")
+    fun retrieveAllMyAccountsCvs(): List<CvResponseDTO> {
+        return cvService.retrieveAllMyAccountsCvs()
+    }
+
+}
--- backend/jobsearch/src/main/kotlin/com/jobsearch/repository/CvRepository.kt
+++ backend/jobsearch/src/main/kotlin/com/jobsearch/repository/CvRepository.kt
@@ -1,6 +1,10 @@
 package com.jobsearch.repository
 
 import com.jobsearch.entity.Cv
+import com.jobsearch.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
-interface CvRepository: JpaRepository<Cv, Int>
+interface CvRepository: JpaRepository<Cv, Int> {
+    fun findByUser(user: User): List<Cv>
+}
