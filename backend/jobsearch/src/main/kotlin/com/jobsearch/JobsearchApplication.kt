55119f0387f323110585edbd6ad49eef71133def
Santisu <esantibra@gmail.com>
2024-03-08 23:03:47 -0300

    add default admin, manger and candidate after run app

--- backend/jobsearch/src/main/kotlin/com/jobsearch/JobsearchApplication.kt
+++ backend/jobsearch/src/main/kotlin/com/jobsearch/JobsearchApplication.kt
@@ -1,10 +1,14 @@
 package com.jobsearch
 
+import com.jobsearch.dto.UserDTO
+import com.jobsearch.service.UserService
+import org.springframework.beans.factory.InitializingBean
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
 class JobsearchApplication {
@@ -16,4 +20,41 @@ class JobsearchApplication {
 
 fun main(args: Array<String>) {
 	runApplication<JobsearchApplication>(*args)
+}
+
+@Component
+class DefaultUserCreator(
+	val userService: UserService
+): InitializingBean {
+	override fun afterPropertiesSet() {
+		val admin = UserDTO(
+			firstName = "Lenny",
+			lastName = "Lennard",
+			email = "lenny@l.com",
+			password = "lenny1",
+			roleId = 3
+		)
+		val manager = UserDTO(
+			firstName = "Carl",
+			lastName = "Carlson",
+			email = "carl@c.com",
+			password = "carl1",
+			roleId = 2
+		)
+		val candidate = UserDTO(
+			firstName = "Juan",
+			lastName = "Topo",
+			email = "juan@t.com",
+			password = "juan1",
+			roleId = 1
+		)
+		for (user in listOf(admin, manager, candidate)) {
+			try {
+				userService.createUser(user)
+			}
+			catch (e: Exception) {
+				println("Default User ${user.email} already in db")
+			}
+		}
+	}
 }
