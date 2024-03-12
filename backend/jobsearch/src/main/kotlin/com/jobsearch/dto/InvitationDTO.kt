0390cce64543ebc752243842bbee45d5c40f1ded
Natalia Devia <natidevia@gmail.com>
2024-03-12 15:34:05 -0300

    added constraints to invitationDTO

--- backend/jobsearch/src/main/kotlin/com/jobsearch/dto/InvitationDTO.kt
+++ backend/jobsearch/src/main/kotlin/com/jobsearch/dto/InvitationDTO.kt
@@ -1,13 +1,17 @@
 package com.jobsearch.dto
 
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.PastOrPresent
 import java.time.*
 
 class InvitationDTO(
     val id: Int?,
+    @get:NotBlank(message = "Message must not be blank")
     val message: String,
+    @get:PastOrPresent(message = "Time must be valid")
     val time: LocalDateTime = LocalDateTime.now(),
-    val managerId: Int,
-    val candidateId: Int,
-    val vacancyId: Int,
+    val managerId: Int?,
+    val candidateId: Int?,
+    val vacancyId: Int?,
 )
 
