90d51e3a43edefd2e4c60ee72cd4736bacd5137a
s0alken <ropedent@gmail.com>
2024-03-13 20:32:38 -0300

    changed CV controller responses to responseEntity

--- backend/jobsearch/src/main/kotlin/com/jobsearch/controller/MyAccountController.kt
+++ backend/jobsearch/src/main/kotlin/com/jobsearch/controller/MyAccountController.kt
@@ -1,8 +1,10 @@
 package com.jobsearch.controller
 
 import com.jobsearch.dto.CvResponseDTO
+import com.jobsearch.response.StandardResponse
 import com.jobsearch.service.CvService
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 
@@ -15,20 +17,44 @@ class MyAccountController(
 
     @GetMapping("cvs/{id}")
     @ResponseStatus(HttpStatus.OK)
-    fun retrieveMyAccountsCv(@PathVariable("id") cvId: Int): CvResponseDTO {
-        return cvService.retrieveMyAccountsCv(cvId)
+    fun retrieveMyAccountsCv(@PathVariable("id") cvId: Int): ResponseEntity<StandardResponse<CvResponseDTO>> {
+        val responseEntity = cvService.retrieveMyAccountsCv(cvId)
+        val status = HttpStatus.OK
+        val body = StandardResponse(
+            status = status.value(),
+            data = responseEntity
+        )
+        return ResponseEntity
+            .status(status)
+            .body(body)
     }
 
     @GetMapping("cvs")
     @ResponseStatus(HttpStatus.OK)
-    fun retrieveAllMyAccountsCvs(): List<CvResponseDTO> {
-        return cvService.retrieveAllMyAccountsCvs()
+    fun retrieveAllMyAccountsCvs(): ResponseEntity<StandardResponse<List<CvResponseDTO>>> {
+        val responseEntityList = cvService.retrieveAllMyAccountsCvs()
+        val status = HttpStatus.OK
+        val body = StandardResponse(
+            status = status.value(),
+            data = responseEntityList
+        )
+        return ResponseEntity
+            .status(status)
+            .body(body)
     }
 
     @GetMapping("cv")
     @ResponseStatus(HttpStatus.OK)
-    fun retrieveMyAccountsLastCv(): CvResponseDTO {
-        return cvService.retrieveMyAccountsLastCv()
+    fun retrieveMyAccountsLastCv(): ResponseEntity<StandardResponse<CvResponseDTO>> {
+        val responseEntity = cvService.retrieveMyAccountsLastCv()
+        val status = HttpStatus.OK
+        val body = StandardResponse(
+            status = status.value(),
+            data = responseEntity
+        )
+        return ResponseEntity
+            .status(status)
+            .body(body)
     }
 
 }
