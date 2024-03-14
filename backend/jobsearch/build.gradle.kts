f043cdeff057c62d4b7d1fe429551637ec1a9992
Santisu <esantibra@gmail.com>
2024-03-14 00:35:05 -0300

    add h2 database for testing

--- backend/jobsearch/build.gradle.kts
+++ backend/jobsearch/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
+	testImplementation("com.h2database:h2:2.2.224")
 }
 
 tasks.withType<KotlinCompile> {
