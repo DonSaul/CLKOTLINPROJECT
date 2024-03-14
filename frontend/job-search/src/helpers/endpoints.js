4708ec54a5b97fd48444bdbc95cc957948b7615e
jamirou <devjamiro@gmail.com>
2024-03-14 10:13:10 -0300

    before pull from dev

--- frontend/job-search/src/helpers/endpoints.js
+++ frontend/job-search/src/helpers/endpoints.js
@@ -7,16 +7,16 @@ const createUrl = (path) => {
 };
 
 export const ENDPOINTS = {
-  register: createUrl("auth/register"),
-  getSkills: createUrl("skills"),
-  submitCV: createUrl("cvs"),
-  login: createUrl("auth/login"),
-  getJobFamilies: createUrl("job-family"),
-  users: createUrl("users"),
-  vacancy: createUrl("vacancy"),
-  searchVacancies: createUrl("vacancy/search"),
-  userCv: createUrl("my-account/cv"),
-  createUser: createUrl("users/create"),
-  searchCandidates: createUrl("candidates/search"),
-  application: createUrl("application")
+  register:createUrl("auth/register"),
+  searchCandidates:createUrl("candidates/search"),
+  getSkills:createUrl("skills"),
+  submitCV:createUrl("cvs"),
+  login:createUrl("auth/login"),
+  getJobFamilies:createUrl("job-family"),
+  users:createUrl("users"),
+  vacancy:createUrl("vacancy"),
+  searchVacancies:createUrl("vacancy/search"),
+  userCv:createUrl("my-account/cv"),
+  createUser:createUrl("users/create"),
+  application:createUrl("application")
 };
