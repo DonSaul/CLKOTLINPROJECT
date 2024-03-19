7febbd8e0f776e14c3a24ed662c48d44ddd766c8
Santisu <esantibra@gmail.com>
2024-03-19 11:06:03 -0300

    add exceptions and handlers

---
+++ backend/jobsearch/src/main/kotlin/com/jobsearch/exception/Exceptions.kt
@@ -0,0 +1,6 @@
+package com.jobsearch.exception
+
+
+class NotFoundException(message: String): RuntimeException(message)
+class UserAlreadyExistsException(message: String): RuntimeException(message)
+class ForbiddenException(message: String): RuntimeException(message)
---
+++ backend/jobsearch/src/main/kotlin/com/jobsearch/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,65 @@
+package com.jobsearch.exception
+
+import com.jobsearch.response.ErrorResponse
+import com.jobsearch.response.StandardResponse
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+
+@RestControllerAdvice
+class GlobalExceptionHandler: ResponseEntityExceptionHandler() {
+    @ExceptionHandler(IllegalArgumentException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleIllegalArgumentException(ex: IllegalArgumentException): ResponseEntity<ErrorResponse<Exception>> {
+        return mapResponseEntity(ex, HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException::class)
+    fun handleUserAlreadyExistsException(ex: UserAlreadyExistsException): ResponseEntity<ErrorResponse<Exception>>{
+        return mapResponseEntity(ex, HttpStatus.CONFLICT)
+    }
+
+    @ExceptionHandler(NotFoundException::class)
+    fun handleNotFoundException(ex: NotFoundException): ResponseEntity<ErrorResponse<Exception>>{
+        return mapResponseEntity(ex, HttpStatus.NOT_FOUND)
+    }
+
+    @ExceptionHandler(IllegalAccessException::class)
+    fun handleIllegalAccessException(ex: IllegalAccessException): ResponseEntity<ErrorResponse<Exception>>{
+        return mapResponseEntity(ex, HttpStatus.FORBIDDEN)
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handleAllException(ex: Exception): ResponseEntity<ErrorResponse<Exception>>{
+        return mapResponseEntity(ex, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+
+    @ExceptionHandler(ForbiddenException::class)
+    fun handleForbiddenException(ex: ForbiddenException): ResponseEntity<ErrorResponse<Exception>>{
+        return mapResponseEntity(ex, HttpStatus.FORBIDDEN)
+    }
+
+    /**
+     * Maps the exception to a standar response.
+     *
+     * @param ex The body of the response.
+     * @param status The HTTP status of the response. Default is HttpStatus.INTERNAL_SERVER_ERROR.
+     * @return The ResponseEntity containing the mapped exception.
+     */
+    fun mapResponseEntity(
+        ex: Exception,
+        status: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR): ResponseEntity<ErrorResponse<Exception>>{
+
+        val bodyContent = ErrorResponse(
+            status = status.value(),
+            data = ex,
+            message = ex.message!!
+        )
+        return ResponseEntity
+            .status(status)
+            .body(bodyContent)
+    }
+}
