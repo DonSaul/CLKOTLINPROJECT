9e3407e0b8faaa32b7632132e7030e8159473e60
Santisu <esantibra@gmail.com>
2024-03-18 21:32:44 -0300

    add candidates/vacancy/{id} endpoint to retrieve candidates by application

--- backend/jobsearch/src/main/kotlin/com/jobsearch/service/VacancyService.kt
+++ backend/jobsearch/src/main/kotlin/com/jobsearch/service/VacancyService.kt
@@ -29,6 +29,7 @@ class VacancyService(
         }
     }
 
+    @Transactional
     fun retrieveVacancyByManager(): List<VacancyResponseDTO> {
         val manager = userService.retrieveAuthenticatedUser()
         return vacancyRepository.findByManager(manager).map {
