6d2854902d4e0b4dbbc9a440bcf9e2fc0ca73c12
Natalia Devia <natidevia@gmail.com>
2024-03-21 19:38:16 -0300

    added invitation view

---
+++ frontend/job-search/src/components/InvitationModal.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: 'none',
+  borderRadius: 5,
+  boxShadow: 24,
+  p: 4,
+};
+
+const InvitationModal = ({children}) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Button mt={2} variant="contained" color="primary" onClick={handleOpen}>Send invitation</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-description" sx={{ m: 2 }}>
+            Please choose the vacancy you would like to send invitations to
+          </Typography>
+            {children}
+        </Box>
+      </Modal>
+    </div>
+  );
+}
+
+export default InvitationModal;
