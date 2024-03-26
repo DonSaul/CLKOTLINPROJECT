051189302d6aee7682c66ad5c769fd38ab5ee57c
Natalia Devia <natidevia@gmail.com>
2024-03-25 21:56:06 -0300

    add profile controller and dto

---
+++ backend/jobsearch/src/main/kotlin/com/jobsearch/controller/ProfileController.kt
@@ -0,0 +1,33 @@
+package com.jobsearch.controller
+
+import com.jobsearch.dto.ProfileDTO
+import com.jobsearch.service.UserService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api/v1/profiles")
+class ProfileController(
+    private val userService: UserService
+) {
+
+     @GetMapping("/my-profile")
+     fun getMyProfileInfo(): ResponseEntity<ProfileDTO> {
+         val currentUser = userService.retrieveAuthenticatedUser()
+         val profileInfo = userService.getUserProfileInfo(currentUser.id!!)
+         return ResponseEntity(profileInfo, HttpStatus.OK)
+    }
+
+    @GetMapping("/{userId}")
+    fun getUserProfileInfo(@PathVariable userId: Int): ResponseEntity<ProfileDTO> {
+        val profileInfo = userService.getUserProfileInfo(userId)
+        return ResponseEntity(profileInfo, HttpStatus.OK)
+    }
+
+    @PutMapping("/{userId}")
+    fun updateUserProfile(@PathVariable userId: Int, @RequestBody updatedProfile: ProfileDTO): ResponseEntity<ProfileDTO> {
+        val updatedInfo = userService.updateUserProfile(userId, updatedProfile)
+        return ResponseEntity(updatedInfo, HttpStatus.OK)
+    }
+}
