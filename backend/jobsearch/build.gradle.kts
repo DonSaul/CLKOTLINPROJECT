b5538227ba5a692842e739d5ebb3586947c9ef46
s0alken <ropedent@gmail.com>
2024-03-27 22:34:55 -0300

    added mapstruct dependency

--- backend/jobsearch/build.gradle.kts
+++ backend/jobsearch/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 	kotlin("jvm") version "1.9.22"
 	kotlin("plugin.spring") version "1.9.22"
 	kotlin("plugin.jpa") version "1.9.22"
+	kotlin("kapt") version "1.9.22"
 }
 
 group = "com.jobsearch"
@@ -57,6 +58,8 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 	testImplementation("com.h2database:h2:2.2.224")
+	kapt("org.mapstruct:mapstruct-processor:1.5.5.Final")
+	implementation("org.mapstruct:mapstruct:1.5.5.Final")
 
 }
 
