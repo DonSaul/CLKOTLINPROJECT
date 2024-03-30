0d8262e2acf66d88b28e213e620e07d722c1a0e7
s0alken <ropedent@gmail.com>
2024-03-30 17:01:12 -0300

    changed notificacion email content of conversation

--- backend/jobsearch/src/main/kotlin/com/jobsearch/repository/ConversationTokenRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.jobsearch.repository
-
-import com.jobsearch.entity.ConversationToken
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface ConversationTokenRepository: JpaRepository<ConversationToken, Int> {
-    fun findByToken(token: String): ConversationToken
-}
