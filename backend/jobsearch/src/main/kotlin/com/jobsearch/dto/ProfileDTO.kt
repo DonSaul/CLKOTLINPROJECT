f4f9fbeb1edee897f74a454e3ab65e9f41e0228a
Natalia Devia <natidevia@gmail.com>
2024-03-30 22:13:48 -0300

    add validations

--- backend/jobsearch/src/main/kotlin/com/jobsearch/dto/ProfileDTO.kt
+++ backend/jobsearch/src/main/kotlin/com/jobsearch/dto/ProfileDTO.kt
@@ -1,7 +1,11 @@
 package com.jobsearch.dto
 
+import jakarta.validation.constraints.NotBlank
+
 class ProfileDTO(
+    @get:NotBlank(message = "First Name must not be blank")
     var firstName: String,
+    @get:NotBlank(message = "Last Name must not be blank")
     var lastName: String,
     var email: String?,
     var roleId: Int,
