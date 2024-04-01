f7b2f564381c85d2377220ecd154304541364734
s0alken <ropedent@gmail.com>
2024-04-01 13:57:39 -0300

    fixed EmailService to be async

--- backend/jobsearch/src/main/kotlin/com/jobsearch/service/EmailService.kt
+++ backend/jobsearch/src/main/kotlin/com/jobsearch/service/EmailService.kt
@@ -1,33 +1,32 @@
 package com.jobsearch.service
 
 import jakarta.mail.MessagingException
-import jakarta.mail.internet.MimeMessage
 import org.springframework.mail.MailException
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.mail.javamail.MimeMessageHelper
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
-import org.thymeleaf.spring6.SpringTemplateEngine
+import java.util.concurrent.CompletableFuture
 
 @Service
-class EmailService(private val mailSender: JavaMailSender, private val templateEngine: SpringTemplateEngine) {
-
+class EmailService(private val mailSender: JavaMailSender) {
+    @Async
     @Throws(MailException::class, MessagingException::class)
-    fun sendEmail(to: String, subject: String, content: String, emailContent: String?) {
+    fun sendEmail(to: String, subject: String?, content: String?, emailContent: String?): CompletableFuture<Void> {
+        return CompletableFuture.runAsync {
+            try {
+                val mimeMessage = mailSender.createMimeMessage()
+                val helper = MimeMessageHelper(mimeMessage, false, "utf-8")
+                helper.setTo(to)
+                helper.setSubject(subject!!)
+                helper.setText((emailContent ?: content)!!, true)
 
-        try {
-            val mimeMessage: MimeMessage = mailSender.createMimeMessage()
-            val helper = MimeMessageHelper(mimeMessage, false, "utf-8")
-            helper.setTo(to)
-            helper.setSubject(subject)
-
-            // if not emailContent, just send the plain content email
-            helper.setText(emailContent ?: content, true)
-
-            mailSender.send(mimeMessage)
-            println("Email sent successfully to $to")
-        } catch (ex: Exception) {
-            println("Failed to send email to $to: ${ex.message}")
-            throw ex
+                mailSender.send(mimeMessage)
+                println("Email sent successfully to $to")
+            } catch (ex: Exception) {
+                println("Failed to send email to " + to + ": " + ex.message)
+                throw RuntimeException(ex)
+            }
         }
     }
 }
