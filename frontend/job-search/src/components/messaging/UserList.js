86c9dbe421b1467445d1f115ff3f9c614a7498fb
Edgar <edgar.araya.contreras@gmail.com>
2024-04-01 15:53:24 -0300

    loading spinner for user list

--- frontend/job-search/src/components/messaging/UserList.js
+++ frontend/job-search/src/components/messaging/UserList.js
@@ -9,8 +9,14 @@ import { truncateText } from "../../helpers/funHelpers";
 import { getRoleString } from "../../helpers/constants";
 import Box from "@mui/material/Box";
 import { messagingPageHeight } from "./messagingHelper";
+import LoadingSpinner from "../LoadingSpinner";
 
-export default function UserList({ users, onSelectUser, onSetUserData }) {
+export default function UserList({
+  users,
+  onSelectUser,
+  onSetUserData,
+  isLoadingUserList,
+}) {
   const handleUserSelect = (user) => () => {
     onSelectUser(user?.email);
     onSetUserData(user);
@@ -24,28 +30,37 @@ export default function UserList({ users, onSelectUser, onSetUserData }) {
       >
         <Typography>Users</Typography>
 
-        {users &&
-          users.map((user) => {
-            const labelId = `icon-list-label-${user.id}`;
-            return (
-              <ListItem key={user.id} disablePadding>
-                <ListItemButton
-                  sx={{ display: "flex", gap: "7px" }}
-                  onClick={handleUserSelect(user)}
-                >
-                  <UserAvatar user={user} enableRoleBorder={true}></UserAvatar>
+        {isLoadingUserList ? (
+          <LoadingSpinner></LoadingSpinner>
+        ) : (
+          <>
+            {users &&
+              users.map((user) => {
+                const labelId = `icon-list-label-${user.id}`;
+                return (
+                  <ListItem key={user.id} disablePadding>
+                    <ListItemButton
+                      sx={{ display: "flex", gap: "7px" }}
+                      onClick={handleUserSelect(user)}
+                    >
+                      <UserAvatar
+                        user={user}
+                        enableRoleBorder={true}
+                      ></UserAvatar>
 
-                  <ListItemText
-                    id={labelId}
-                    primary={truncateText(
-                      `${user.firstName} ${user.lastName} `,
-                      20
-                    )}
-                  />
-                </ListItemButton>
-              </ListItem>
-            );
-          })}
+                      <ListItemText
+                        id={labelId}
+                        primary={truncateText(
+                          `${user.firstName} ${user.lastName} `,
+                          20
+                        )}
+                      />
+                    </ListItemButton>
+                  </ListItem>
+                );
+              })}
+          </>
+        )}
       </List>
     </Box>
   );
